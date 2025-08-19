254018ab67d02843941828d4a73d02eba73a2da6
MedovTimur <medov.tr@phystech.edu>
2025-08-19 17:13:21 +0300

    bug

--- vara-app/app/src/lib.rs
+++ vara-app/app/src/lib.rs
@@ -2,20 +2,36 @@
 
 use sails_rs::prelude::*;
 use sails_rs::gstd::msg;
+use gstd::prog::ProgramGenerator;
+use sails_rs::gstd::exec;
 
 struct VaraAppService(());
 
+impl VaraAppService {
+    pub fn init() -> Self {
+        msg::send_with_gas(msg::source(), (), 0, msg::value()).expect("Error during send value");
+        Self(())
+    }
+}
 #[sails_rs::service]
 impl VaraAppService {
     pub fn new() -> Self {
-        msg::send_with_gas(msg::source(), (), 0, msg::value()).expect("Error during send value");
         Self(())
     }
 
     // Service's method (command)
-    pub fn do_something(&mut self) -> String {
-        msg::send_with_gas(msg::source(), (), 0, msg::value()).expect("Error during send value");
-        "Hello from VaraApp!".to_string()
+    pub async fn do_something(&mut self, code_id: CodeId) -> ActorId {
+        let create_program_future = ProgramGenerator::create_program_bytes_with_gas_for_reply(
+            code_id,
+            ("New",  ()).encode(),
+            5_000_000_000,
+            msg::value() - exec::env_vars().existential_deposit,
+            0,
+        )
+        .expect("Error ProgramGenerator");
+
+        let (address, _) = create_program_future.await.expect("Error to create program");
+        address
     }
 
     // Service's query
@@ -30,6 +46,7 @@ pub struct VaraAppProgram(());
 impl VaraAppProgram {
     // Program's constructor
     pub fn new() -> Self {
+        VaraAppService::init();
         Self(())
     }
 
--- vara-app/tests/gtest.rs
+++ vara-app/tests/gtest.rs
@@ -15,59 +15,31 @@ async fn do_something_works() {
     let program_code_id = remoting.system().submit_code(vara_app::WASM_BINARY);
 
     let program_factory = vara_app_client::VaraAppFactory::new(remoting.clone());
-    let balance = remoting.system().balance_of(ACTOR_ID);
-    println!("balance: {balance:?}");
 
     let program_id = program_factory
-        .new() // Call program's constructor (see app/src/lib.rs:29)
-        .with_value(10_000_000_000_000)
+        .new()
         .send_recv(program_code_id, b"salt")
         .await
         .unwrap();
 
     let mut service_client = vara_app_client::VaraApp::new(remoting.clone());
 
-    let balance = remoting.system().balance_of(ACTOR_ID);
-    println!("balance: {balance:?}");
+    let program_id_balance = remoting.system().balance_of(program_id);
+    println!("\n 1. balance: {program_id_balance:?} \n");
 
     let result = service_client
-        .do_something() // Call service's method (see app/src/lib.rs:14)
-        .with_value(5_000_000_000_000)
+        .do_something(program_code_id)
+        .with_value(20_000_000_000_000)
         .send_recv(program_id)
         .await
         .unwrap();
 
-    let balance = remoting.system().balance_of(ACTOR_ID);
-    println!("balance: {balance:?}");  
+    let program_id_balance = remoting.system().balance_of(program_id);
+    println!("\n 2. balance: {program_id_balance:?} \n");
 
-    assert_eq!(result, "Hello from VaraApp!".to_string());
-}
+    let new_program_id_balance = remoting.system().balance_of(result);
+    println!("\n 3. balance: {new_program_id_balance:?} <- new program\n");
 
-#[tokio::test]
-async fn get_something_works() {
-    let system = System::new();
-    system.init_logger_with_default_filter("gwasm=debug,gtest=info,sails_rs=debug");
-    system.mint_to(ACTOR_ID, 100_000_000_000_000);
-    let remoting = GTestRemoting::new(system, ACTOR_ID.into());
-
-    // Submit program code into the system
-    let program_code_id = remoting.system().submit_code(vara_app::WASM_BINARY);
-
-    let program_factory = vara_app_client::VaraAppFactory::new(remoting.clone());
-
-    let program_id = program_factory
-        .new() // Call program's constructor (see app/src/lib.rs:29)
-        .send_recv(program_code_id, b"salt")
-        .await
-        .unwrap();
-
-    let service_client = vara_app_client::VaraApp::new(remoting.clone());
-
-    let result = service_client
-        .get_something() // Call service's query (see app/src/lib.rs:19)
-        .recv(program_id)
-        .await
-        .unwrap();
-
-    assert_eq!(result, "Hello from VaraApp!".to_string());
+    assert_eq!(new_program_id_balance, 1000000000000);
+    assert_eq!(program_id_balance,     20000000000000);
 }
